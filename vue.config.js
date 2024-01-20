bbabcb34ff1e69099403e1b25514d62adff9cb30
守夜人 <123456789@qq.com>
2024-01-20 13:25:39 +0800

    添加gzip, 添加esbuild-loader

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,56 @@
 const { defineConfig } = require('@vue/cli-service')
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = defineConfig({
+  publicPath: './',
   transpileDependencies: true,
-  chainWebpack: (config) => {
-    config.entry('code').clear().add('./src/code.js')
-    config.entry('app').clear().add('./src/main.js')
-  },
+
   configureWebpack: () => {
     return {
       entry: {
         code: './src/code.js',
       },
-      optimization: {
-        splitChunks: {
-          chunks: 'all',
-          // minSize: 1024 * 5,
-          // maxSize: 1024 * 20,
-        },
-      },
+
       plugins: [
         new MomentLocalesPlugin({
           // 剥离除 “zh-cn” 以外的所有语言环境。
           localesToKeep: ['zh-cn'],
         }),
+        new CompressionPlugin({
+          test: /\.js$/,
+          threshold: 1024 * 50,
+        }),
       ],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'js',
+              target: 'es2022',
+            },
+          },
+        ],
+      },
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 1024 * 5,
+          maxSize: 1024 * 20,
+        },
+      },
     }
   },
   pages: {
-    index: {
+    app: {
       entry: './src/main.js',
+      filename: 'index.html',
+      template: './public/index.html',
+      title: 'vue2',
       chunks: ['code', 'chunk-vendors', 'chunk-common', 'app'],
+      chunksSortMode: 'manual',
     },
   },
 })
