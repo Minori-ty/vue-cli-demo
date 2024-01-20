dbf29db423361fba46228ad0ead8468bba615b71
守夜人 <123456789@qq.com>
2024-01-20 09:17:21 +0800

    配置成功

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import moment from 'moment'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data() {
+    return {
+      moment,
+    }
+  },
+  created() {
+    console.log(this.moment().format('YYYY MM DD HH:mm:ss'))
+  },
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,44 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.entry('code').clear().add('./src/code.js')
+    config.entry('app').clear().add('./src/main.js')
+  },
+  configureWebpack: () => {
+    return {
+      entry: {
+        code: './src/code.js',
+      },
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          // minSize: 1024 * 5,
+          // maxSize: 1024 * 20,
+          cacheGroups: {
+            moment: {
+              name: 'monemt-test',
+              priority: 20,
+              test: /[/]node_modules[/]_?moment(.*)/,
+              enforce: true,
+            },
+          },
+        },
+      },
+      plugins: [
+        new webpack.IgnorePlugin({
+          resourceRegExp: /moment[/\\]locale$/,
+          contextRegExp: /zh-cn/,
+        }),
+      ],
+    }
+  },
+  pages: {
+    index: {
+      entry: './src/main.js',
+      chunks: ['code', 'chunk-vendors', 'chunk-common', 'app'],
+    },
+  },
 })
